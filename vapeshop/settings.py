f32278b77190a898ca8eb11a5cc62f6b38683c9a
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-09-18 09:30:17 +0200

    set up ready for heroku deployment

--- vapeshop/settings.py
+++ vapeshop/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pp5-vapeshop.herokuapp.com', 'localhost']
 
 
 # Application definition
