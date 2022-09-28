fe84222769aa5c854640e73b99eb70857643250f
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-09-28 22:38:00 +0200

    Create base template in templates folder and create layout folder with head file to include in the base.html

--- vapeshop/settings.py
+++ vapeshop/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['pp5-vapeshop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pp5-vapeshop.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
