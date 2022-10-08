52d7837e7e90b0951a89697bee57aab6e8a131e9
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-10-09 00:59:36 +0200

    make price updade when changing has sale from admin panel

--- products/admin.py
+++ products/admin.py
@@ -4,39 +4,78 @@ from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModel
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Category, Product, DisposableVapes
 
-# Register your models here.
+# if time come back and create promo sale functions i,e 10% off all products
+
+
+def start_sale(self, request, queryset):
+    """"
+    Call back function to start a sale in when called from the admin panel
+    created to avoid having to manually change the price of each product
+    """
+
+    queryset.update(has_sale=True)
+    for obj in queryset:
+        obj.price = obj.discounted_price
+        obj.save()
+
+
+def end_sale(self, request, queryset):
+    queryset.update(has_sale=False)
+    for obj in queryset:
+        obj.price = obj.rrp
+        obj.save()
 
 
 class ModelAChildAdmin(PolymorphicChildModelAdmin, SummernoteModelAdmin):
     """ Base admin class for all child models """
     base_model = Product
 
+
 @admin.register(DisposableVapes)
 class ProductAdmin(ModelAChildAdmin):
     base_model = DisposableVapes
     show_in_index = True
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'rrp',
+        'price',
+        'current_rating',
+        'has_sale',
+        'image')
+    search_fields = ('name', 'brand', 'description', 'category__friendly_name')
+    actions = ['add_sale', 'remove_sale']
+
+    def add_sale(self, request, queryset):
+        start_sale(self, request, queryset)
+
+    def remove_sale(self, request, queryset):
+        end_sale(self, request, queryset)
+
 
 @admin.register(Product)
 class ProductAdmin(PolymorphicParentModelAdmin):
     base_model = Product
     child_models = (DisposableVapes,)
     summer_fields = ('description',)
-    list_display = ('sku', 'name', 'category', 'rrp', 'price', 'current_rating', 'has_sale', 'image')
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'rrp',
+        'price',
+        'current_rating',
+        'has_sale',
+        'image')
     search_fields = ('name', 'brand', 'description', 'category__friendly_name')
     actions = ['add_sale', 'remove_sale']
 
     def add_sale(self, request, queryset):
-        queryset.update(has_sale=True)
-        for obj in queryset:
-            obj.price = obj.discounted_price
-            obj.save()
-        
+        start_sale(self, request, queryset)
 
     def remove_sale(self, request, queryset):
-        queryset.update(has_sale=False)
-        for obj in queryset:
-            obj.price = obj.rrp
-            obj.save()
+        end_sale(self, request, queryset)
 
 
 @admin.register(Category)
