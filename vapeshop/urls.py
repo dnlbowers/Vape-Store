a19c3f3433347e8772c720e7ac9c9dda8c65e736
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-10-09 02:21:19 +0200

    migrate recent models

--- vapeshop/urls.py
+++ vapeshop/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
