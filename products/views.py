f6355346d1a4e56141613c463cf2ee738199f58c
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-10-16 01:15:54 +0200

    plan out nav bar menu options

--- products/views.py
+++ products/views.py
@@ -2,28 +2,34 @@ from django.shortcuts import get_object_or_404, render, reverse, redirect
 from django.views import generic, View  # noqa
 from django.contrib import messages
 from django.db.models import Q
+# from .models import CategoryGroupings, AllProducts
 from .models import Accessories, AllProducts, BaseLiquids, Batteries
 from .models import DisposableVapes, FlavorConcentrates, Mods, NicotineShots
 from .models import PreBuiltCoils, Tanks, VapeJuice, CategoryGroupings
 
 
 # Keep til later in case needed
-# class ProductGroupsMixin(View):
-#     ALL_PRODUCTS = [
-#         Accessories,
-#         BaseLiquids,
-#         Batteries,
-#         DisposableVapes,
-#         FlavorConcentrates,
-#         Mods,
-#         NicotineShots,
-#         PreBuiltCoils,
-#         Tanks,
-#         VapeJuice
-#     ]
-#     product_list = []
-#     for product in ALL_PRODUCTS:
-#         product_list.append(product.objects.all())
+class ProductGroupsMixin(View):
+    ALL_PRODUCTS = [
+        Accessories,
+        BaseLiquids,
+        Batteries,
+        DisposableVapes,
+        FlavorConcentrates,
+        Mods,
+        NicotineShots,
+        PreBuiltCoils,
+        Tanks,
+        VapeJuice
+    ]
+    product_list = []
+    for product in ALL_PRODUCTS:
+        product_list.append(product.objects.all())
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = self.product_list
+        return context
 
 
 class AllProductsView(generic.ListView):
@@ -39,6 +45,7 @@ class AllProductsView(generic.ListView):
 
     def get_queryset(self, *args, **kwargs):
         qs = super(AllProductsView, self).get_queryset(*args, **kwargs)
+        # products = super(ProductGroupsMixin, self).get_context_data(**kwargs)
 
         if 'category' in self.request.GET:
             self.categories = self.request.GET['category'].split(',')
@@ -84,8 +91,8 @@ class ProductDetails(View):
         individual_product = get_object_or_404(AllProducts, id=id)
 
         context = {
-                    'product': individual_product
-                }
+            'product': individual_product
+        }
         return render(
             request,
             'products/product_detail.html', context)
