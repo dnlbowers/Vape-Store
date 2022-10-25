8287c4cf20178a1a534f3a7430adc5007b14a91c
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-10-25 21:45:53 +0200

    draft cart view for mobile devices

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -8,13 +8,13 @@ from products.models import AllProducts
 def cart_contents(request):
 
     cart_contents = []
-    total = 0
+    cart_total = 0
     product_count = 0
     cart = request.session.get('cart', {})
 
     for product_id, quantity in cart.items():
         product = get_object_or_404(AllProducts, pk=product_id)
-        total += quantity * product.price
+        cart_total += quantity * product.price
         product_count += quantity
         cart_contents.append({
             'product_id': product_id,
@@ -22,23 +22,23 @@ def cart_contents(request):
             'product': product
             })
 
-    if total < settings.FREE_SHIPPING_QUALIFIER:
-        shipping = total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE / 100)
-        free_shipping_delta = settings.FREE_SHIPPING_QUALIFIER - total
+    if cart_total < settings.FREE_SHIPPING_QUALIFIER:
+        shipping = cart_total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE / 100)
+        free_shipping_delta = settings.FREE_SHIPPING_QUALIFIER - cart_total
     else:
         shipping = 0
         free_shipping_delta = 0
 
-    cart_total = shipping + total
+    grand_total = shipping + cart_total
 
     context = {
         'cart_contents': cart_contents,
-        'total': total,
+        'cart_total': cart_total,
         'product_count': product_count,
         'shipping': shipping,
         'free_shipping_delta': free_shipping_delta,
         'free_shipping_qualifier': settings.FREE_SHIPPING_QUALIFIER,
-        'cart_total': cart_total,
+        'grand_total': grand_total,
 
     }
 
