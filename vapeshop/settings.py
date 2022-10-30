89caacaf478b5ddd2df2206ffdd351dabdb8abff
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-10-30 13:59:09 +0100

    add checkout app, and add optional no image image

--- vapeshop/settings.py
+++ vapeshop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # other
     'storages',
     'django_summernote',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'shopping_cart.contexts.cart_contents'
             ],
         },
