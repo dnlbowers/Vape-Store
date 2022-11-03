52a0b73236f71539a2cf7f816bff0a61b717824b
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-11-03 22:10:23 +0100

    fix file path for stripe elements js, add some styling to stripe ecard element, make view more in line with OOP princlibles

--- checkout/views.py
+++ checkout/views.py
@@ -13,15 +13,18 @@ class Checkout(View):
 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+    payment_form = PaymentForm()
+    template = 'checkout/checkout.html'
 
     def get(self, request, *args, **kwargs):
         cart = request.session.get('cart', {})
         if not cart:
             messages.error(request, "Your cart is currently empty")
             return redirect(reverse('products'))
-        payment_form = PaymentForm()
-        template = 'checkout/checkout.html'
+
         context = {
-            'payment_form': payment_form,
+            'payment_form': self.payment_form,
+            'stripe_public_key': self.stripe_public_key,
+            
         }
-        return render(request, template, context)
+        return render(request, self.template, context)
