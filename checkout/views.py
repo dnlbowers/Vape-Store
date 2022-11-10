75caf82623ef74c45c615501ef313e4d0b79454d
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-11-10 16:51:02 +0100

    fix django-counties flag being pushed to AWS

--- checkout/views.py
+++ checkout/views.py
@@ -21,11 +21,9 @@ def cache_checkout_data(request):
         stripe.api_key = settings.STRIPE_SECRET_KEY
 
         stripe.PaymentIntent.modify(payment_id, metadata={
-
             'cart': json.dumps(request.session.get('cart', {})),
             'save_info': request.POST.get('save_info'),
             # 'username': request.user,
-
         })
 
         return HttpResponse(status=200)
