c5cbf9e6827ca4c1dd615030692680e7c8b0a9f9
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-11-11 15:00:39 +0100

    tidy up templates, remove unused nav links, and link profile template to show draft profile

--- profiles/views.py
+++ profiles/views.py
@@ -6,8 +6,8 @@ from .models import UserProfile
 class Profile(View):
 
     def get(self, *args, **kwargs):
-        # profile = get_object_or_404(UserProfile, user=self.request.user)
+        profile = get_object_or_404(UserProfile, user=self.request.user)
         context = {
-            # 'profile': profile,
+            'profile': profile,
         }
         return render(self.request, 'profiles/profile.html', context)
