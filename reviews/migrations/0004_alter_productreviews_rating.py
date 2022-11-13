9c9a77c91ae8371ece85a0e213f139af5f98f085
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-11-14 00:12:08 +0100

    start to make FA symbols small

--- reviews/migrations/0004_alter_productreviews_rating.py
+++ reviews/migrations/0004_alter_productreviews_rating.py
@@ -14,6 +14,9 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='productreviews',
             name='rating',
-            field=models.PositiveIntegerField(validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(5)]),
+            field=models.PositiveIntegerField(
+                validators=[
+                    django.core.validators.MinValueValidator(1),
+                    django.core.validators.MaxValueValidator(5)]),
         ),
     ]
